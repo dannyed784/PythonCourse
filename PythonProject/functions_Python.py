fa7e9bf78a7d38e30165df2b0ac1bd45ee01111d
Daniel <dannyed784@gmail.com>
2025-02-22 12:32:52 -0500

    Day 8 Functions

---
+++ PythonProject/functions_Python.py
@@ -0,0 +1,82 @@
+#Day 8: FUNCTION PARAMETERS AND CAESAR CIPHER
+from ast import literal_eval
+
+
+#Create a function called greet()
+#Write 3 print statements inside the function.
+#Call the greet() function and run your code.
+
+# def greet(name,lastName,profession):
+#     print(f"Hello I am {name} {lastName} and I "
+#           f"work as a {profession}")
+#
+# greet("Daniel","Moreno","Programmer analyst")
+
+# Code challenge #7: Life in weeks
+# def life_in_weeks(age):
+#     weeks_you_lived = age*52
+#     weeks_remaining = 4680 - weeks_you_lived
+#     print(f"You have {weeks_remaining} weeks left.")
+#
+# life_in_weeks(20)
+
+#62 Positional and keyword arguments
+# def greet_with(name="Jason",location="Nowhere"):
+#     print(f"Hello {name}")
+#     print(f"What is is like in {location}")
+#
+# greet_with()
+
+# Coding challenge #8: Love calculator
+
+# def calculate_love_score(name1,name2):
+#     names = (name1 + name2).lower()
+#     sum1 = 0
+#     sum2 = 0
+#     print(names)
+#     for letter in names:
+#         if letter in "true":
+#             sum1+=1
+#         if letter in "love":
+#             sum2+=1
+#         else:
+#             sum1+=0
+#             sum2+=0
+#     return f"{str(sum1)}"+f"{str(sum2)}"
+#
+# print(calculate_love_score("Kanye West","Kim Kardashian"))
+# print(calculate_love_score("Brad Pitt","Jennifer Aniston"))
+# print(calculate_love_score("Prince Willam","Kate Middleton"))
+
+
+#63 Caesar Cipher Part 1 - Python
+
+# 1. Create function called 'encrypt()' that takes 'original_text' and shift amount
+# as 2 inputs
+
+# 2. Inside the 'encrypt()' function, shift each letter of the 'original_text' forwards
+# in the alphabet by the shift amount and print the encrypted text.
+
+# 3. you can use the built-in Python index() function to find out the position of an item
+# in a list
+
+# 4. What happens if you try to shift z forward by 9? Can you fix te code?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
